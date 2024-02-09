de6fe636db551c0533c0ceff45e5d439f5e4c1c7
razergliri <glicojerico@gmail.com>
2024-02-09 20:58:31 +0800

    Committing all files to the repository

---
+++ Section 3/AskJava.java
@@ -0,0 +1,8 @@
+public class AskJava {
+    public static void main(String[] args) {
+        System.out.println("Hi Java, do I have enough to buy chips?");
+        double change = 3.50;
+        double price = 2.50;
+        System.out.println("Java: " + (change >= price) + "\n");
+    }
+}
