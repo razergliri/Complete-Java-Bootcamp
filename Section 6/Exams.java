946ee6e282fb2ad25cd7033d437baeb7a1706116
razergliri <glicojerico@gmail.com>
2024-02-14 17:39:48 +0800

    Added Folder Section 6 for Array java file exercises

---
+++ Section 6/Exams.java
@@ -0,0 +1,8 @@
+public class Exams {
+    public static void main(String[] args) {
+        String[] seats = {"Harry", "Neville", "Ron", "Hermione", "Seamus"};
+        for(int i = 0; i <seats.length; i++){
+            System.out.println(seats[i] + ", you will take seat " + i );
+        }
+    }
+}
---
+++ Section 6/IntroToArrays.java
@@ -0,0 +1,9 @@
+public class IntroToArrays {
+    public static void main(String[] args) {
+        
+        String[] kingdoms = {"Mercia","Wessex","Northumbria","East Anglia"};
+        for(int i = 0; i <kingdoms.length; i++){
+            System.out.println(kingdoms[i]);
+        }
+    }
+}
