45aba69bd2a4706d7d651cd60a51b0734293ea3e
razergliri <glicojerico@gmail.com>
2024-02-15 23:09:43 +0800

    Added Javapedia challenge

---
+++ Section 6/Javapedia.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Javapedia {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("**********Javapedia**********");
+        System.out.println("How many historical figures will you register?");
+        int figures = 0;
+        System.out.print("Your answer:");
+        figures = scan.nextInt();
+    }
+}
