917768e4e63021573e842763ccc06ed488873c22
razergliri <glicojerico@gmail.com>
2024-02-20 22:00:15 +0800

    Private access field exercise and getters

--- Section 7/Car.java
+++ Section 7/Car.java
@@ -1,8 +1,8 @@
 public class Car {
-    String make;
-    double price;
-    int year;
-    String color;
+    private String make;
+    private double price;
+    private int year;
+    private String color;
     
 
     public Car(String make, double price, int year, String color){
@@ -10,5 +10,18 @@ public class Car {
         this.price = price;
         this.year = year;
         this.color = color;
+    }
+
+    public String getMake(){
+        return make;
+    }
+    public double getPrice(){
+        return price;
+    }
+    public int getYear(){
+        return year;
+    }
+    public String getColor(){
+        return color;
     }
 }
--- Section 7/Main.java
+++ Section 7/Main.java
@@ -8,21 +8,17 @@ public class Main{
         Car dodge = new Car("Dodge", 11000, 2019, "Blue");
         
 
-        System.out.println("This " + nissan.make + " is worth $" + nissan.price + 
-        ". It was built in " + nissan.year + ". It is " + nissan.color + ".\n");
-        System.out.println("This " + dodge.make + " is worth $" + dodge.price + 
-        ". It was built in " + dodge.year + ". It is " + dodge.color + ".\n");
+        System.out.println("This " + nissan.getMake() + " is worth $" + nissan.getPrice() + 
+        ". It was built in " + nissan.getYear() + ". It is " + nissan.getColor() + ".\n");
+        System.out.println("This " + dodge.getMake() + " is worth $" + dodge.getPrice() + 
+        ". It was built in " + dodge.getYear() + ". It is " + dodge.getColor() + ".\n");
         
-        Person person = new Person();
-        person.name = "Shamel Glico";
-        person.nationality = "Filipino";
-        person.dateOfBirth = "06/16/2004";
+        Person person = new Person("Shamel", "Filipino", "06/16/2004", 7);
+        
         person.passport = new String[] {person.name, person.nationality, person.dateOfBirth};
-        person.seatNumber = 7;
-        System.out.println(person.name);
-        System.out.println(person.nationality);
-        System.out.println(person.dateOfBirth);
-        System.out.println(Arrays.toString(person.passport));
-        System.out.println(person.seatNumber);
+        
+        System.out.println("Name: " + person.name + "\n" + "Nationality: " + person.nationality
+            + "\n" + "Date of Birth: " + person.dateOfBirth + "\n" + "Seat Number: "
+            + person.seatNumber + "\n");
     }
 }
