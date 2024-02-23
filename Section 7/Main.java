c0f63dc17ee6ba9f45389e56884869431c80753a
razergliri <glicojerico@gmail.com>
2024-02-23 08:21:57 +0800

    Completed the Dealership application with OOP

--- Section 7/Main.java
+++ Section 7/Main.java
@@ -1,29 +1,30 @@
-import java.util.Arrays;
+import java.util.Scanner;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
-    Person person = new Person("Uncle Sam", "Australian", "04/04/4444", 2);
 
-    System.out.println("Name: " + person.getName() + "\n" + "Nationality: " + person.getNationality() + "\n"
-    + "Date of Birth: " + person.getDateOfBirth() + "\n" + "Seat Number: " + person.getSeatNumber() + "\n"
-    );
-    if(person.applyPassport()){
-        person.setPassport();
-        System.out.println("Passport: " + Arrays.toString(person.getPassport()) + "\n");
-    }else{
-        System.out.println("Passport: " + Arrays.toString(person.getPassport()) + "\n");
-    }
-       
-    
-    System.out.println("break");
-    Car nissan = new Car("Nissan", 10000, 2020, "Green", new String[] {"Tires", "Keys"} );
-    Car dodge = new Car("dodge", 11000, 2019, "Blue", new String[] {"Tires", "Keys"} ); 
-    Car nissan2 = new Car(nissan);
-    
-    System.out.println(nissan);
-    System.out.println(dodge);
-    System.out.println(nissan2);
-    System.out.println(person);
+        Scanner scan = new Scanner(System.in);
+        
+        Car[] cars = new Car[] {
+            new Car("Nissan", 5000, 2020, "red", new String[] {"tires", "keys"}),
+            new Car("Dodge", 8500, 2019, "blue", new String[] {"tires", "keys"}),
+            new Car("Nissan", 5000, 2017, "yellow", new String[] {"tires", "filter"}),
+            new Car("Honda", 7000, 2019, "orange", new String[] {"tires", "filter"}),
+            new Car("Mercedes", 12000, 2015, "jet black", new String[] {"tires", "filter", "transmission"})
+        };
+        
+        Dealership dealership = new Dealership(cars);
+        
+
+        System.out.println("\n ****** JAVA DEALERSHIP! ****** \n");        
+        System.out.println("Feel free to browse through our collection of cars.\n");
+        System.out.println(dealership);
+        System.out.println("Which car are you interested in? (0 - 4).\n");
+        int index = scan.nextInt();
+        dealership.sell(index);
+        // sell car here... 
+
+        scan.close();
 
     }
 }
