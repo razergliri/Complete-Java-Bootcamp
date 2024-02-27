efea3efc31bcfa5719a8badf015581b70f3bae4d
razergliri <glicojerico@gmail.com>
2024-02-27 17:07:57 +0800

    modified section 7 extra practice for part 3 completion

--- Section 7/Extra Practice/Main.java
+++ Section 7/Extra Practice/Main.java
@@ -1,15 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
 
-
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         Contact contact1 = new Contact("Alice", "123-456-7890", "1990-01-01");
         Contact contact2 = new Contact("Bob", "234-567-8901", "1992-02-02");
-        Contact contact3 = new Contact("Charlie", "345-678-9012", "1994-03-03");
-        Contact contact4 = new Contact("David", "456-789-0123", "1996-04-04");
-    
-        Contact copyContact1 = new Contact(contact1);
-        Contact copyContact2 = new Contact(contact3);
-        contact1.setBirthDate("1989-01-01");
-    
+        
+        Contact[] contacts = {contact1, contact2};
+        ContactManager contactManager = new ContactManager(contacts);
+
+        Contact retrievedContact = contactManager.getContact(0);
+        System.out.println(retrievedContact.getName());
+
+        Contact newContact  = new Contact("Charlie", "345-678-9012", "1994-03-03");
+        contactManager.setContact(newContact, 0);
+
+        retrievedContact = contactManager.getContact(0);
+        System.out.println(retrievedContact.getName());
+        scanner.close();
     }
 }
