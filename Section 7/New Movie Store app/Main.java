9c348b08dea0a343f29d0f699a2ac94fb7fe8693
razergliri <glicojerico@gmail.com>
2024-02-29 16:34:56 +0800

    Added New folder for movie store and app modified it to latest

--- Section 7/New Movie Store app/Main.java
+++ Section 7/New Movie Store app/Main.java
@@ -1,51 +1,52 @@
 import java.util.Scanner;
 
-public class Main{
+public class Main {
+
+
     static Store store = new Store();
     public static void main(String[] args) {
         Movie[] movies = new Movie[]{
             new Movie("The Shawshank Redemption", "BlueRay", 9.2),
             new Movie("The Godfather", "BlueRay", 9.1),
             new Movie("The Godfather: Part II", "DVD", 9.0),
+            new Movie("12 Angry Men", "DVD", 8.9),
             new Movie("The Dark Knight", "BlueRay", 9.0),
             new Movie("Schindler's List", "DVD", 8.9),
             new Movie("The Lord of the Rings: The Return of the King", "BlueRay", 8.9),
             new Movie("Pulp Fiction", "DVD", 8.8),
+            new Movie("The Good, the Bad and the Ugly", "DVD", 8.8),
             new Movie("The Lord of the Rings: The Fellowship of the Ring", "DVD", 8.8)
         };
-
-
-        System.out.println("********************************Glico's Movie Ratings*******************************");
-       
-
-      
-        for(int i = 0; i < movies.length; i++){
-            store.setMovies(i, movies[i]);
-        }
-        for(int i = 0; i < movies.length; i++){
-            System.out.println(movies[i]);
-        }
-       userInput();
-       for(int i = 0; i < movies.length; i++){
-        System.out.println(movies[i]);
+        
+       for(Movie movie : movies){
+        store.addMovie(movie);
+       }
+        printStore();
+        userInput();
+        printStore();
     }
+
+
+    public static void printStore(){
+        System.out.println("******GLICOS RATING******");
+        System.out.println(store);
     }
+
     public static void userInput(){
         Scanner scanner = new Scanner(System.in);
         String status = "continue";
         while (status.equals("continue")) {
-            System.out.println("\nPlease enter an integer between 0-9");
+            System.out.print("\nPlease enter an integer between 0-9: ");
             int choice = scanner.nextInt();
-            Movie movie = store.getMovies(choice);
-            System.out.println("Set a new rating for " + movie.getName() +": ");
+            Movie movie = store.getMovie(choice);
+            System.out.print("Set a new rating for " + movie.getName() +": ");
             double rating = scanner.nextDouble();
             movie.setRating(rating);
-            store.setMovies(choice, movie);
+            store.setMovie(choice, movie);
             
-            System.out.println("To edit another rating, type: 'continue': "); scanner.nextLine();
+            System.out.print("To edit another rating, type: 'continue': "); scanner.nextLine();
             status=scanner.nextLine();
         }
         scanner.close();
     } 
-    
 }
