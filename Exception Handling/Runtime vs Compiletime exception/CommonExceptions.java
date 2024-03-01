4573ccd3d1b2a4146ea1ef39e9ba534ecbb311da
razergliri <glicojerico@gmail.com>
2024-03-01 17:08:33 +0800

    added runtime exceptions file exercise

---
+++ Exception Handling/Runtime vs Compiletime exception/CommonExceptions.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class CommonExceptions {
+    public static void main(String[] args) {
+        
+        //********************************ArrayIndexOutOfBounds**************************//
+        int[] grades = new int[] {96, 65, 56, 86};
+        System.out.println(grades[3]);
+
+           //********************************NullPointerException**************************//
+        String[] names = new String[7];
+        names[0] = "John";
+        names[1] = "Jim";
+        names[2] = "Joe";
+
+        for(String name : names){
+            if(name != null){
+                System.out.println(name.toUpperCase());
+            }
+           
+        }
+        
+        //********************************InputMismatch Exception**************************//
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter a random integer: ");
+        if(scanner.hasNextInt()){
+            scanner.nextInt();
+        }else{
+            scanner.next();
+        }
+        
+        scanner.close();
+    }
+}
