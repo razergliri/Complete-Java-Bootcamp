6ed83c829fc5a416ed724b14cc3bf2e5ff991230
razergliri <glicojerico@gmail.com>
2024-03-09 20:38:07 +0800

    Added a exercise file on enums

---
+++ ENUM/Main.java
@@ -0,0 +1,8 @@
+import model.Car;
+import static model.Car.TrafficLight;
+public class Main{
+    public static void main(String[] args) {
+            Car car = new Car("Nissan", 2020);
+            car.drive(Car.TrafficLight.GREEN);
+    }
+}
